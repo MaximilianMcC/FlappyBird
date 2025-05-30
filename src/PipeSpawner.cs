6ebcad2ac1e78574f8d8bc4e364041840768e1cb
MaximilianMcC <max@maximilian.co.nz>
2025-05-31 01:29:02 +1200

    kinda began working on pipe spawner

---
+++ src/PipeSpawner.cs
@@ -0,0 +1,17 @@
+using System;
+using Smoke;
+
+class PipeSpawner : UpdatableComponent
+{
+	private Timer timer => GameObject.Get<Timer>();
+
+	public override void Update()
+	{
+		// Check for if we can spawn a pipe
+		if (GameManager.Paused) return;
+		if (timer.Done == false) return;
+
+		// Spawn a pipe
+		Console.WriteLine("Spawned pipe");
+	}
+}
