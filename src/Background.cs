6b0a076935151df64d2acf8e1b95f56a8ed440fc
MaximilianMcC <max@maximilian.co.nz>
2025-05-31 00:30:48 +1200

    foreground on the background

--- src/Background.cs
+++ src/Background.cs
@@ -6,24 +6,33 @@ using static Smoke.Runtime;
 
 class Background : RenderableComponent
 {
-	public float ScrollSpeed = -100;
+	public float ScrollSpeed = -50;
 	private float skyPosition = 0;
+	private float groundPosition = 0;
+
+	// TODO: Jst use a transform
+	private Vector2 groundSize;
 
 	public override void LoadType()
 	{
-		// Load the sky background image
+		// Load the background images
 		Textures["sky"] = LoadTexture("./assets/image/sky.png");
+		Textures["ground"] = LoadTexture("./assets/image/ground.png");
+
+		groundSize = new Vector2(WindowWidth, WindowHeight / 4);
 	}
 
 	public override void Update()
 	{
-		// Move the background across the screen
+		// Move the backgrounds across the screen
 		skyPosition += ScrollSpeed * DeltaTime;
+		groundPosition += (ScrollSpeed * 2f) * DeltaTime;
 
-		// When the 'first' sky goes fully out of
+		// When the 'first' one goes fully out of
 		// frame then teleport it back to the start
 		// so it looks like its in an infinite loop
 		if (skyPosition <= -WindowWidth) skyPosition = 0;
+		if (groundPosition <= -WindowWidth) groundPosition = 0;
 	}
 
 	public override void Render2D()
@@ -31,5 +40,9 @@ class Background : RenderableComponent
 		// Draw both skies
 		DrawTexture(Textures["sky"], new Vector2(skyPosition, 0), WindowSize);
 		DrawTexture(Textures["sky"], new Vector2(skyPosition + WindowWidth, 0), WindowSize);
+
+		// Draw both grounds
+		DrawTexture(Textures["ground"], new Vector2(groundPosition, WindowHeight - groundSize.Y), groundSize);
+		DrawTexture(Textures["ground"], new Vector2(groundPosition + WindowWidth, WindowHeight - groundSize.Y), groundSize);
 	}
 }
